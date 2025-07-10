87a8cc244d7bc9b01ca42e30c1a4f1021d6b296b
bikenewby <sudduenk@gmail.com>
2025-07-11 03:35:39 +0700

    Changing data persistance from EEPROM Emulator to FRAM

---
+++ Debug/Core/Src/sh2/euler.d
@@ -0,0 +1,3 @@
+Core/Src/sh2/euler.o: ../Core/Src/sh2/euler.c \
+ C:/Users/Krit/STM32CubeIDE/workspace/Test_STM32F411CEU6_SSD1306_BNO085INT_SBUS_FreeRTOS/Core/Inc/sh2/euler.h
+C:/Users/Krit/STM32CubeIDE/workspace/Test_STM32F411CEU6_SSD1306_BNO085INT_SBUS_FreeRTOS/Core/Inc/sh2/euler.h:
